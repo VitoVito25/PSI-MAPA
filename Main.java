705c6a70ee8a571c6fe57c82aa22af27ca4247ba
Victor Tomazoni Renaud <victortomazonir@gmail.com>
2024-09-26 20:43:15 -0300

    clearConsole() create

--- Main.java
+++ Main.java
@@ -1,5 +1,20 @@
 public class Main {
     
+    public static void clearConsole() {
+        try {
+            if (System.getProperty("os.name").contains("Windows")) {
+                // Comando para limpar o console no Windows
+                new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+            } else {
+                // Comando para limpar o console no Unix/Linux/MacOS
+                new ProcessBuilder("clear").inheritIO().start().waitFor();
+            }
+        } catch (Exception e) {
+            System.out.println("Erro ao limpar o console.");
+            e.printStackTrace();
+        }
+    }
+    
     public static void main(String[] args) {
 
         TicTacToe game = new TicTacToe();
--- TicTacToe.java
+++ TicTacToe.java
@@ -47,4 +47,6 @@ public class TicTacToe {
         }
     }
 
+    public
+
 }
