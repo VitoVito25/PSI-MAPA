1092de41b6a6255a9fd4237bcdde4a74034ca80d
Victor Tomazoni Renaud <victortomazonir@gmail.com>
2024-09-28 09:17:51 -0300

    Jogo 100% funcional

--- Main.java
+++ Main.java
@@ -21,27 +21,60 @@ public class Main {
     public static void main(String[] args) {
         
         Scanner scanner = new Scanner(System.in);  // Inicia o Scanner para leitura do teclado
-        TicTacToe game = new TicTacToe(); // Inicia uma instancia do Jogo
-        boolean gameFinished = false, gameFinishedDraw = false;
-        game.setPositionsBoard();
+        boolean loopGame = true;
 
-        clearConsole();
-        game.printFirstMenu(); // Mostra o menu principal
-        scanner.nextLine(); // Aguarda o usuario a clicar ENTER
-        clearConsole();
+        do{           
+            TicTacToe game = new TicTacToe(); // Inicia uma instancia do Jogo
+            boolean gameFinished = false, gameFinishedDraw = false;   
+            int inGameNumber = 0;
+            game.setPositionsBoard();
 
-        game.resetBoard();
-
-        do{
-            game.startTurn(scanner);
+            //Primeiro Menu
             clearConsole();
-            gameFinished = game.verifyWin();
+            game.printFirstMenu(); // Mostra o menu principal
+            scanner.nextLine(); // Aguarda o usuario a clicar ENTER
+            clearConsole();
 
-            if(gameFinished == false) {
-                gameFinishedDraw = game.verifyDraw();
-            }       
-              
-        } while(gameFinished == false && gameFinishedDraw == false);        
+            game.resetBoard();
+
+            // Loop do jogo
+            do{
+                game.startTurn(scanner);
+                clearConsole();
+                gameFinished = game.verifyWin();
+
+                if(gameFinished == false) {
+                    gameFinishedDraw = game.verifyDraw();
+                }       
+
+            } while(gameFinished == false && gameFinishedDraw == false);  
+            
+            // Indica o final do jogo
+            if(gameFinished == true) {
+                game.printBoard();
+                game.changeMarker();
+                System.err.println("\n \nO jogador com o marcador " + game.marker + " ganhou a partida! ");
+            }else if(gameFinishedDraw == true) {
+                game.printBoard();
+                System.out.println("\n \nO jogo deu empate!");
+            }
+
+            // Pergunta ao usuario se quer jogar novamente
+            try {
+                System.out.println("\nPressione 1 para jogar novamente ou 9 para sair");
+                inGameNumber = scanner.nextInt();
+            }catch (InputMismatchException e) {
+                System.out.println("Você inseriou um caracter invalido, pressione ENTER para reiniciar o jogo...");
+                scanner.nextLine();
+            }
+            
+            // Verifica a saida
+            if(inGameNumber == 9) {
+                loopGame = false;
+            }else {
+                loopGame = true;
+            }
+        }while(loopGame == true);
 
         scanner.close(); // Fecha o Scanner
 
